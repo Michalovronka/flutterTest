c1b5006a2c83ef4da7b2c6f848e7fcf4f9e06210
Michalovronka <kokosnahlave123@seznam.cz>
2025-10-23 13:56:35 +0200

    backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todoapp/sharedstate.dart';
@@ -5,12 +8,45 @@ import 'package:todoapp/task.dart';
 import 'package:todoapp/todo.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   List<Task> tasks = [];
-  final prefs = await SharedPreferences.getInstance();
+  try {
+    final response = await http.get(
+      Uri.parse('http://127.0.0.1:8000/todos'),
+    );
+    if (response.statusCode == 200) {
+      Map json = jsonDecode(response.body);
+
+      /*for (var i = 1; i < json.length; i++) {
+        var value = json['$i'];
+        print(value['title']);
+
+        tasks.add(Task(name: "e", isCompleted: false));
+      }*/
+
+      json.forEach((key, value) {
+        print(value['title']);
+        tasks.add(
+          Task(
+            name: value['title'],
+            isCompleted: value['isCompleted'],
+          ),
+        );
+      });
+    } else {
+      print('Error: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('HTTP request failed: $e');
+  }
+
+  /*final prefs = await SharedPreferences.getInstance();
   for (var i = 0; i < (prefs.getInt('numberOfTasks') ?? 0); i++) {
     tasks.add(Task(name: i.toString(), isCompleted: false));
-  }
-  runApp(SharedState(tasks: tasks, color: Colors.red, child: MyApp()));
+  }*/
+  runApp(
+    SharedState(tasks: tasks, color: Colors.red, child: MyApp()),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -32,12 +68,10 @@ class _MyAppState extends State<MyApp> {
       setState(() {
         prefs.setInt('numberOfTasks', numberOfTasks);
       });
-      SharedState.of(context).tasks.removeRange(0,SharedState.of(context).tasks.length);
-      for (
-        var i = 0;
-        i < (prefs.getInt('numberOfTasks') ?? 0);
-        i++
-      ) {
+      SharedState.of(
+        context,
+      ).tasks.removeRange(0, SharedState.of(context).tasks.length);
+      for (var i = 0; i < (prefs.getInt('numberOfTasks') ?? 0); i++) {
         tasks.add(Task(name: i.toString(), isCompleted: false));
       }
       SharedState.of(context).tasks.addAll(tasks);
@@ -67,7 +101,10 @@ class _MyAppState extends State<MyApp> {
                 maxLength: 20,
               ),
             ),
-            TextButton(onPressed: addNumberOfTasks, child: (Text("Save"))),
+            TextButton(
+              onPressed: addNumberOfTasks,
+              child: (Text("Save")),
+            ),
             Expanded(
               child: ListView.builder(
                 key: const Key('long_list'),
